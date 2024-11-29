22b803f59f7e76eea8e129b137f16ce5044a6a23
Craig <CMPeoples3@gmail.com>
2024-11-29 09:53:55 -0500

    inital work w/ uuid

--- eventBus.go
+++ eventBus.go
@@ -1,25 +1,38 @@
 package main
 
-import "fmt"
-
-type Listeners struct {
-	id string
-	ip string
-}
+import (
+	"github.com/google/uuid"
+)
 
 type EventBus struct {
-	registry map[string][]Listeners
+	registry map[uuid.UUID]*Consumer
 }
 
-func (eb *EventBus) _topicExist(topic string) bool {
-	_, ok := eb.registry[topic]
-	return ok
+// 1 : 1 event bus. Messages are posted with a subscriber in mind
+func NewEventBus() *EventBus {
+	reg := make(map[uuid.UUID]*Consumer)
+	return &EventBus{
+		registry: reg,
+	}
 }
 
-func (eb *EventBus) Publish(topic, message string) error {
-	if !eb._topicExist(topic) {
-		return fmt.Errorf("Topic has not been registerred")
+func (eb *EventBus) RegisterConsumer(con *Consumer) {
+	if eb.registry[con.ID] != nil {
+		return
 	}
 
-	
+	eb.registry[con.ID] = con
 }
+
+// If we want to do some sort of grouped routing topics like this would be useful
+// func (eb *EventBus) _topicExist(topic string) bool {
+// 	_, ok := eb.registry[topic]
+// 	return ok
+// }
+
+// func (eb *EventBus) Publish(topic, message string) error {
+// 	if !eb._topicExist(topic) {
+// 		return fmt.Errorf("Topic has not been registerred")
+// 	}
+
+// }
